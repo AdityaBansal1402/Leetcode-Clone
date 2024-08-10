c88e9745825a903bff03f50fa14bc047e83f6988
Aditya Bansal <bansaladitya3009@gmail.com>
2024-08-10 23:33:44 +0530

    gonna add backend

---
+++ Backend/Hope/src/main/java/com/example/demo/HopeApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HopeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HopeApplication.class, args);
+	}
+
+}
