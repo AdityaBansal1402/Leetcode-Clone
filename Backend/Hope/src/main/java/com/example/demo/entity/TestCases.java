9cda2d82578bda98c57835cb79bf7f2560a4de1c
Aditya Bansal <bansaladitya3009@gmail.com>
2024-09-02 05:05:10 +0530

    hello

--- Backend/Hope/src/main/java/com/example/demo/entity/TestCases.java
+++ Backend/Hope/src/main/java/com/example/demo/entity/TestCases.java
@@ -1,15 +1,14 @@
 package com.example.demo.entity;
 
-import com.vladmihalcea.hibernate.type.json.JsonBinaryType;
+//import com.vladmihalcea.hibernate.type.json.JsonBinaryType;
+import com.vladmihalcea.hibernate.type.json.JsonType;
+
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
-import org.hibernate.annotations.JdbcTypeCode;
 import org.hibernate.annotations.Type;
-import org.hibernate.type.SqlTypes;
 //import org.hibernate.annotations.TypeDef;
 
 @Getter
@@ -18,16 +17,20 @@ import org.hibernate.type.SqlTypes;
 @AllArgsConstructor
 @Entity
 @Table(name = "testcases")
-@Convert(attributeName = "entityAttrName", converter = StringJsonUserType.class)
+//@TypeDef(name = "jsonb", typeClass = JsonBinaryType.class)
 public class TestCases {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @JdbcTypeCode(SqlTypes.JSON)
+    @Type(JsonType.class)
     @Column(columnDefinition = "jsonb", name = "input")
     private Object inputDataObject;
+    
+    @Type(JsonType.class)
+    @Column(columnDefinition = "jsonb", name = "input")
+    private Object outputDataObject;
 
     @ManyToOne
     @JoinColumn(name = "problem_id")
